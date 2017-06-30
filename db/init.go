6660372f52c95a384e115575ac9191d773f3fc59
lempiy <lempiyada@gmail.com>
2017-06-30 23:32:35 +0300

    Add heroku

--- db/init.go
+++ db/init.go
@@ -14,8 +14,11 @@ var database db.Database
 var err error
 
 func init() {
-	info := fmt.Sprintf("user=%s password=%s dbname=%s host=postgres port=5432 sslmode=disable",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DB"))
+	info := os.Getenv("DATABASE_URL")
+	if info == "" {
+		info = fmt.Sprintf("user=%s password=%s dbname=%s host=postgres port=5432 sslmode=disable",
+			os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DB"))
+	}
 	database.DB, err = sql.Open("postgres", info)
 	if err != nil {
 		fmt.Print(err)
--- models/models.go
+++ models/models.go
@@ -12,8 +12,11 @@ var Database db.Database
 var err error
 
 func init() {
-	info := fmt.Sprintf("user=%s password=%s dbname=%s host=postgres port=5432 sslmode=disable",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DB"))
+	info := os.Getenv("DATABASE_URL")
+	if info == "" {
+		info = fmt.Sprintf("user=%s password=%s dbname=%s host=postgres port=5432 sslmode=disable",
+			os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DB"))
+	}
 	Database.DB, err = sql.Open("postgres", info)
 	if err != nil {
 		fmt.Print(err)
